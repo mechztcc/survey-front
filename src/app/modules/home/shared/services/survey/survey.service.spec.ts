597f2bacc97cb93304fa7df43e5a9c8eda505c95
mechztcc <dojathan@gmail.com>
2024-01-04 07:07:41 -0500

    feat: first test to onCreate survey

--- src/app/modules/home/shared/services/survey/survey.service.spec.ts
+++ src/app/modules/home/shared/services/survey/survey.service.spec.ts
@@ -1,16 +1,46 @@
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
-
 import { SurveyService } from './survey.service';
 
 describe('SurveyService', () => {
   let service: SurveyService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [SurveyService],
+    });
     service = TestBed.inject(SurveyService);
+    httpMock = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpMock.verify();
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  fit('should create a survey', () => {
+    const mockPayload = { question: 'Test question', expiresAt: new Date() };
+    const mockResponse = {
+      id: 1,
+      ...mockPayload,
+      createdAt: '2024-01-04T12:00:00Z', 
+      updatedAt: '2024-01-04T12:00:00Z',
+    };
+
+    service.onCreate(mockPayload).subscribe((response) => {
+      expect(response).toEqual(mockResponse);
+    });
+
+    const req = httpMock.expectOne('survey');
+    expect(req.request.method).toBe('POST');
+    req.flush(mockResponse);
   });
 });
